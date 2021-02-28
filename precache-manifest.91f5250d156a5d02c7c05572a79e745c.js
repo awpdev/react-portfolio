e24aba40e3fcca82a7b717b3f65a9efcb4f461ec
Andrew Park <apark999@gmail.com>
2021-02-28 14:08:30 -0800

    Updates

---
+++ precache-manifest.91f5250d156a5d02c7c05572a79e745c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a354065d4b8d1b3e6fd937fb11310e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dae8217e918b1258301b",
+    "url": "/static/css/2.bdc49607.chunk.css"
+  },
+  {
+    "revision": "0ffdf5200ca46e5d8e48",
+    "url": "/static/css/main.2a42203c.chunk.css"
+  },
+  {
+    "revision": "dae8217e918b1258301b",
+    "url": "/static/js/2.0d2bd761.chunk.js"
+  },
+  {
+    "revision": "0ffdf5200ca46e5d8e48",
+    "url": "/static/js/main.09e34ebb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0165461ba512519f118df6a72aec168f",
+    "url": "/static/media/821.0165461b.jpg"
+  },
+  {
+    "revision": "28a4c66e6f287600449c1e397a0da6dc",
+    "url": "/static/media/budget.28a4c66e.png"
+  },
+  {
+    "revision": "5a62f0ccc81caa3d0f089833867e2963",
+    "url": "/static/media/demo1.5a62f0cc.gif"
+  },
+  {
+    "revision": "a29941a58938192378cf283c0ff65f0a",
+    "url": "/static/media/javascriptquizgif1.a29941a5.gif"
+  },
+  {
+    "revision": "ffb359c58b25c025c867d536300f9ca9",
+    "url": "/static/media/notetakerdemo.ffb359c5.gif"
+  },
+  {
+    "revision": "2c59c56ceb1a32a2bef34d31b0b4422e",
+    "url": "/static/media/readme1.2c59c56c.jpg"
+  },
+  {
+    "revision": "e4de1c488bfc21bb0109d98eb112d550",
+    "url": "/static/media/teamhtml.e4de1c48.png"
+  }
+]);
