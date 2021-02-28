8247f50982afeb5059776aeea490d48318ded338
Andrew Park <apark999@gmail.com>
2021-02-28 15:10:47 -0800

    Updates

---
+++ precache-manifest.5f23c61f8c81088e7c2231e8a0451d12.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7cac42c2ac94ad9221e8427e3d5d81",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dae8217e918b1258301b",
+    "url": "/react-portfolio/static/css/2.bdc49607.chunk.css"
+  },
+  {
+    "revision": "286611a8cc5c6dd3ef2b",
+    "url": "/react-portfolio/static/css/main.c22d714c.chunk.css"
+  },
+  {
+    "revision": "dae8217e918b1258301b",
+    "url": "/react-portfolio/static/js/2.0d2bd761.chunk.js"
+  },
+  {
+    "revision": "286611a8cc5c6dd3ef2b",
+    "url": "/react-portfolio/static/js/main.f0753b47.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "0165461ba512519f118df6a72aec168f",
+    "url": "/react-portfolio/static/media/821.0165461b.jpg"
+  },
+  {
+    "revision": "28a4c66e6f287600449c1e397a0da6dc",
+    "url": "/react-portfolio/static/media/budget.28a4c66e.png"
+  },
+  {
+    "revision": "5a62f0ccc81caa3d0f089833867e2963",
+    "url": "/react-portfolio/static/media/demo1.5a62f0cc.gif"
+  },
+  {
+    "revision": "a29941a58938192378cf283c0ff65f0a",
+    "url": "/react-portfolio/static/media/javascriptquizgif1.a29941a5.gif"
+  },
+  {
+    "revision": "ffb359c58b25c025c867d536300f9ca9",
+    "url": "/react-portfolio/static/media/notetakerdemo.ffb359c5.gif"
+  },
+  {
+    "revision": "2c59c56ceb1a32a2bef34d31b0b4422e",
+    "url": "/react-portfolio/static/media/readme1.2c59c56c.jpg"
+  },
+  {
+    "revision": "e4de1c488bfc21bb0109d98eb112d550",
+    "url": "/react-portfolio/static/media/teamhtml.e4de1c48.png"
+  }
+]);
